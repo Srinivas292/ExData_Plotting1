9a464bcc5cec40b88b97aea294fc6a9520871344
Srinivas292 <69464587+Srinivas292@users.noreply.github.com>
2020-10-13 21:42:50 +0530

    Update Assignment_1.R

--- Assignment_1.R
+++ Assignment_1.R
@@ -19,10 +19,18 @@ power <- power[which(power$Date == '2/2/2007' | power$Date=='1/2/2007'),]
 
 power$POSIX <-as.POSIXlt.character(paste(power$Date,power$Time),format = "%d/%m/%Y %H:%M:%S")
 
+
+
+
+
 #plot.1
 png(filename="plot1.png",width=480, height=480)
 hist(power$Global_active_power, col = 'red', main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)')
 dev.off()
+
+
+
+
 
 #plot2
 png(filename="plot2.png",width=480, height=480)
